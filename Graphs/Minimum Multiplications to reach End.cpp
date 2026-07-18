e6b2b3076adf5f6714770ff85fc08f5c43c48062
SiddharthDhumal <siddharthdhumal210@gmail.com>
2026-07-19 00:00:28 +0530

    adding Minimum Multiplications to reach End

---
+++ Graphs/Minimum Multiplications to reach End.cpp
@@ -0,0 +1,49 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+class Solution {
+  public:
+    int minSteps(vector<int>& arr, int start, int end) {
+        // code here
+        
+        if(start==end) return 0;
+        
+        int mod = 1000;
+        
+        queue<pair<int,int>>q;
+        
+        
+        vector<int>distance(mod,INT_MAX);
+        
+        distance[start] = 0;
+        
+        q.push({0,start});
+        
+        
+        while(!q.empty()){
+            int step = q.front().first;
+            int node = q.front().second;
+            q.pop();
+            
+            if(node==end) return step;
+            
+            if(step>distance[node])
+                continue;
+            
+            for(int i=0;i<arr.size();i++){
+                int nextStep = step+1;
+                int newVal = (node * arr[i])%mod;
+                
+                if(nextStep < distance[newVal]){
+                    q.push({nextStep,newVal});
+                
+                    distance[newVal] = nextStep;
+                }
+                
+           
+            }
+        }
+        
+        return -1;
+    }
+};
