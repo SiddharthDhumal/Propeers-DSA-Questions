57d665a65524c994f1ef635cec38a117349a8a3f
SiddharthDhumal <siddharthdhumal210@gmail.com>
2026-08-05 01:53:34 +0530

    adding 0-1 knapsack probelm

--- DP/dp_0-1-knapsack.cpp
+++ DP/dp_0-1-knapsack.cpp
@@ -1,6 +1,69 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Sample Input:
+// 1 
+// 4
+// 1 2 4 5
+// 5 4 8 6
+// 5
+// Sample Output:
+// 13
+
+// dfs(4,5)
+// │
+// ├── Take Item4 (value=6)
+// │      │
+// │      └── dfs(3,0)
+// │              │
+// │              └── return 0
+// │
+// │      Total = 6
+// │
+// └── Don't Item4
+//        │
+//        └── dfs(3,5)
+//              │
+//              ├── Take Item3 (value=8)
+//              │      │
+//              │      └── dfs(2,1)
+//              │              │
+//              │              └── dfs(1,1)
+//              │                     │
+//              │                     ├── Take
+//              │                     │      └── dfs(0,0)=0
+//              │                     │
+//              │                     └── Don't
+//              │                            └── dfs(0,1)=0
+//              │
+//              │      Total = 8+5=13
+//              │
+//              └── Don't Item3
+//                     │
+//                     └── dfs(2,5)
+//                            │
+//                            ├── Take Item2 (value=4)
+//                            │      │
+//                            │      └── dfs(1,3)
+//                            │             │
+//                            │             ├── Take
+//                            │             │      └── dfs(0,2)=0
+//                            │             │
+//                            │             └── Don't
+//                            │                    └── dfs(0,3)=0
+//                            │
+//                            │      Total=9
+//                            │
+//                            └── Don't Item2
+//                                   │
+//                                   └── dfs(1,5)
+//                                          │
+//                                          ├── Take
+//                                          │      └── dfs(0,4)=0
+//                                          │
+//                                          └── Don't
+//                                                 └── dfs(0,5)=0
+
 int dfs(int index,int remainingWeight,const vector<int>& weight,
 const vector<int>& value,vector<vector<int>>& dp){
    if(index==0 || remainingWeight==0)
